1d4ced1563812f59e5365be71eb2ec0dd65381e2
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-02-11 22:23:52 +0200

    task 2

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./Header";
+import Footer from "./Footer";
+import MainContent from "./MainContent";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -30,6 +33,9 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   );
 }
