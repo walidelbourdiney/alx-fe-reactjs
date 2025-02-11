907ea9543e5743314f37e2a6e423d43814a4380b
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-02-11 22:33:51 +0200

    task 3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./Header";
 import Footer from "./Footer";
 import MainContent from "./MainContent";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -36,6 +37,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   );
 }
