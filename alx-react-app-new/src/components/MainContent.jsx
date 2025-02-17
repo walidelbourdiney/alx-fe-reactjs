046545615564d93379aa824542320e51f7223f22
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-02-17 19:33:26 +0200

    week 10 task 0 init

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+function MainContent() {
+  const style = {
+    backgroundColor: "blue",
+  };
+  return (
+    <main style={style}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
