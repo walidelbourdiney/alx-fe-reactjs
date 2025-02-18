493f5ad412255b2c570bd2fd6fb12a8630794ab5
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-02-18 10:44:12 +0200

    useRef practice

--- alx-react-app-new/src/components/WelcomeMessage.jsx
+++ alx-react-app-new/src/components/WelcomeMessage.jsx
@@ -1,14 +1,27 @@
-import { useContext } from "react";
+import { useContext, useRef, useState } from "react";
 import { data } from "../App";
 
 function WelcomeMessage() {
   const dataa = useContext(data);
+  const [count1, setCount1] = useState(0);
+  const element = useRef();
+  function handleClick() {
+    console.log(element);
+    element.current.style.width = "300px";
+  }
   return (
     <div>
       <h1>Hello everyone, I am learning React at ALX!</h1>
       <p>This is a simple JSX component.</p>
       <p>I am learning about JSX!</p>
       <p>the count is {dataa} </p>
+      <input
+        ref={element}
+        type="text"
+        onChange={() => setCount1((prev) => prev + 1)}
+      />
+      <button onClick={handleClick}>click</button>
+      <p>{count1}</p>
     </div>
   );
 }
