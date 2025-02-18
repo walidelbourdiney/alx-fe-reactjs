fb11f1b1270dc456f717387f7f52b59da4cd3347
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-02-18 10:18:51 +0200

    practice useContext

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, createContext } from "react";
 
 import "./App.css";
 import WelcomeMessage from "./components/WelcomeMessage";
@@ -7,10 +7,16 @@ import Footer from "./components/Footer";
 import MainContent from "./components/MainContent";
 import UserProfile from "./components/UserProfile";
 
+export const data = createContext();
+
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <>
-      <WelcomeMessage />
+      <data.Provider value={count}>
+        <WelcomeMessage />
+      </data.Provider>
+
       <Header />
       <MainContent />
       <Footer />
--- alx-react-app-new/src/components/WelcomeMessage.jsx
+++ alx-react-app-new/src/components/WelcomeMessage.jsx
@@ -1,9 +1,14 @@
+import { useContext } from "react";
+import { data } from "../App";
+
 function WelcomeMessage() {
+  const dataa = useContext(data);
   return (
     <div>
       <h1>Hello everyone, I am learning React at ALX!</h1>
       <p>This is a simple JSX component.</p>
       <p>I am learning about JSX!</p>
+      <p>the count is {dataa} </p>
     </div>
   );
 }
