d4bdabda94ef40e6b390507d7a932d9809d8f87c
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-02-19 11:03:24 +0200

    editttt

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import UserContext from "./UserContext";
-import UserContext from "./UserContext";
-function UserProfile(props) {
+import { useContext } from "react";
+function UserProfile() {
+  const { name, email } = useContext(UserContext);
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
+      <h2>{name}</h2>
+      <p>Age: {age}</p>
       <p>Bio: {props.bio}</p>
     </div>
   );
