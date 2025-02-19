0e2ce5b0b795afae858105c225a45893dc23eb10
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-02-19 17:44:19 +0200

    z

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
-import Services from "./Services";
-import Contact from "./Contact";
-import Navbar from "./Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <BrowserRouter>
