5d6c81dae10019cee8b4cbef748d2608769de7ce
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-03-02 14:59:27 +0200

    --

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {RecipeList} from './components/RecipeList';
-import {AddRecipeForm} from './components/AddRecipeForm';
-import {RecipeDetails} from './components/RecipeDetails';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
 import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList';
