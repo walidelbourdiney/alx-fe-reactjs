48cf6f4727cdc392e134a95d3d7e401856aacc68
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-03-11 16:20:41 +0200

    edit typography

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -10,7 +10,7 @@ function UserProfile() {
       <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-3 sm:my-4">
         John Doe
       </h1>
-      <p className="text-sm sm:text-base text-gray-600">
+      <p className="text-sm sm:text-base text-gray-600 md:text-xl">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
