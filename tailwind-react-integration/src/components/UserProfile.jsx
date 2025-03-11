7ba03bd01a21a4cc0c732dc5800809ecf2015e0d
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-03-11 16:17:08 +0200

    task 2

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,16 @@
 import src from "../src/assets/react.svg";
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
-      <img src={src} alt="User" className="w-36 h-36 rounded-full mx-auto" />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">
+    <div className="bg-gray-100 p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-16 md:my-20 rounded-lg shadow-lg text-center">
+      <img
+        src={src}
+        alt="User"
+        className="w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 rounded-full mx-auto"
+      />
+      <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-3 sm:my-4">
+        John Doe
+      </h1>
+      <p className="text-sm sm:text-base text-gray-600">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
