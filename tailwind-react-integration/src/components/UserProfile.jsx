d7145b8dc43b7a84e964f488351f8ec4bb98d2a1
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-03-11 16:27:35 +0200

    3

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,13 @@
 import src from "../src/assets/react.svg";
 function UserProfile() {
   return (
-    <div className="bg-gray-100 sm:p-4 sm:p-6 md:p-8 max-w-xs md:max-w-sm mx-auto my-10 sm:my-16 md:my-20 rounded-lg shadow-lg text-center sm:w-24 sm:h-24">
+    <div className=" hover:shadow-xl transition-shadow duration-300 ease-in-out bg-gray-100 sm:p-4 sm:p-6 md:p-8 max-w-xs md:max-w-sm mx-auto my-10 sm:my-16 md:my-20 rounded-lg shadow-lg text-center sm:w-24 sm:h-24">
       <img
         src={src}
         alt="User"
-        className="w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 rounded-full mx-auto"
+        className="w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-3 sm:my-4">
+      <h1 className=" hover:text-blue-500 transition-colors duration-300 ease-in-out text-lg sm:text-xl md:text-2xl text-blue-800 my-3 sm:my-4">
         John Doe
       </h1>
       <p className="text-sm sm:text-base text-gray-600 md:text-xl">
