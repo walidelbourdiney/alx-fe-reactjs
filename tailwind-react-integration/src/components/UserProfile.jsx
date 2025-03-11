35b6e1c3b768dc1cb9704b995e1929d53e318395
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-03-11 16:25:38 +0200

    sa

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,17 +1,16 @@
-import src from "../assets/react.svg";
-
+import src from "../src/assets/react.svg";
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-6 md:p-8 max-w-xs md:max-w-sm mx-auto my-10 sm:my-16 md:my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out text-center">
+    <div className="bg-gray-100 sm:p-4 sm:p-6 md:p-8 max-w-xs md:max-w-sm mx-auto my-10 sm:my-16 md:my-20 rounded-lg shadow-lg text-center sm:w-24 sm:h-24">
       <img
         src={src}
         alt="User"
-        className="w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
+        className="w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 rounded-full mx-auto"
       />
-      <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-3 sm:my-4 hover:text-blue-500 transition-colors duration-300 ease-in-out">
+      <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-3 sm:my-4">
         John Doe
       </h1>
-      <p className="text-sm sm:text-base text-gray-600">
+      <p className="text-sm sm:text-base text-gray-600 md:text-xl">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
